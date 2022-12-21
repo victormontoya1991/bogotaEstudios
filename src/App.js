c442c52cb3630fcd97265932f660012b5a47316a
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-21 09:33:29 -0500

    construccion theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import { PageHeader } from './components/PageHeader/Pageheader';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
         <PageHeader />
+        <ItemListContainer />
     </div>
   );
 }
--- src/components/SearchWindow/SearchWindow.js
+++
@@ -1,9 +0,0 @@
-import './SearchWindow.scss';
-
-export const SearchWindow = () => {
-    return(
-        <div className="SearchWindow">
-            <input type="text" placeholder="Buscar"/>
-        </div>
-    )
-}
